45f95f87da8831376a7d08514562ff44cb7cf7c4
Ramez Taher <ramezgamer11@gmail.com>
2022-09-17 18:16:40 +0100

    creating header and adding sass to the project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,10 +16,28 @@ const Header = () => {
     }
     getMovies()
   }, [])
-  console.log(movie)
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str
+  }
   return (
-    <header className="bg-gray">
-      <div className="container sm:mx-auto px-4">header</div>
+    <header
+      className="text-white object-contain h-[470px] bg-center bg-cover"
+      style={{
+        backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.5)),
+        url(${imageBaseUrl}${movie?.backdrop_path})`,
+      }}
+    >
+      <div className="container sm:mx-auto px-4 pt-16 flex flex-col gap-4">
+        <h1 className="text-3xl font-extrabold ">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div>
+          <button className="btn">Play</button>
+          <button className="btn">My List</button>
+        </div>
+
+        <p className="w-1/2">{truncate(movie?.overview, 170)}</p>
+      </div>
     </header>
   )
 }
