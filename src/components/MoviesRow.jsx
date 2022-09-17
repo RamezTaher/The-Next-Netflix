4d2de7ac70d63303cc164c53cb30b77611a08835
Ramez Taher <ramezgamer11@gmail.com>
2022-09-17 19:39:28 +0100

    trailer added

--- src/components/MoviesRow.jsx
+++ src/components/MoviesRow.jsx
@@ -1,11 +1,36 @@
+import movieTrailer from "movie-trailer"
 import React, { useState } from "react"
 import { useEffect } from "react"
+import YouTube from "react-youtube"
 import fetcher from "../api/axios"
 
 const imageBaseUrl = "https://image.tmdb.org/t/p/original/"
 
 const MoviesRow = ({ title, endpoint }) => {
   const [movies, setMovies] = useState([])
+  const [trailerUrl, setTrailerUrl] = useState("")
+  const options = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  }
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("")
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search)
+          setTrailerUrl(urlParams.get("v"))
+        })
+        .catch((error) => console.log(error))
+    }
+  }
+
+  console.log(trailerUrl)
 
   useEffect(() => {
     const getMovies = async () => {
@@ -22,6 +47,7 @@ const MoviesRow = ({ title, endpoint }) => {
       <div className="flex gap-2 overflow-x-scroll overflow-y-hidden py-6 scrollbar-hide">
         {movies.map((movie) => (
           <div
+            onClick={() => handleClick(movie)}
             key={movie.id}
             className={` w-full  transition hover:scale-[1.09] flex-[0_0_50%] md:flex-[0_0_25%] lg:flex-[0_0_20%] xl:flex-[0_0_10%]  ${
               title === "Netflix Originals" ? "h-[250px]" : "h-[100px]"
@@ -35,6 +61,7 @@ const MoviesRow = ({ title, endpoint }) => {
           </div>
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={options} />}
     </div>
   )
 }
